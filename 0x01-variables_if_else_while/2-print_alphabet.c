b9d61686e6b491d7e016235b17ac8f6eed5a13e4
Mjay08 <amah.mary18@gmail.com>
2022-10-17 09:40:56 -0700

    changes to script 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,16 +14,15 @@
 int main(void)
 
 {
-
-	int ch;
+	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 
-		putchar(ch);
-
-	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
 
 		putchar(ch);
+
+	}
 
 	putchar('\n');
 
